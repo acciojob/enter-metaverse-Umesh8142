a25def0f61c12f4210df3b9b7a9bef76388a01f8
N UMESH <102941752+Umesh8142@users.noreply.github.com>
2023-10-19 11:04:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+const btn =document.getElementById("enterBtn");
+btn.addEventListener("click", function() {
+    const p =document.getElementById("status");
+	p.innerText="Entered Metaverse"
+});
